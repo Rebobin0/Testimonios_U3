2eb5f375edb2ad34ea0a356002a6846670369fac
Emiliano Rebolledo <rebolledonavarreteemiliano@gmail.com>
2025-11-06 12:38:15 -0600

    Segundo commit. Se puede visualizar en la pantalla los testimonios aunque de forma estatica

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const testimonios = [
+    { 
+        id: 1, 
+        nombre: 'Ana Pérez', 
+        cargo: 'CEO - EmpresaX', 
+        texto: 'Este servicio cambió la forma en que trabajamos. ¡Altamente recomendado!', 
+        foto: 'https://i.pravatar.cc/150?img=32' 
+    }, 
+    { id: 2, 
+        nombre: 'Luis García', 
+        cargo: 'Freelancer', 
+        texto: 'Me ayudó a duplicar mis clientes. Fácil de usar y fiable.', 
+        foto: 'https://i.pravatar.cc/150?img=12' 
+    }, 
+    { 
+        id: 3, 
+        nombre: 'María López', 
+        cargo: 'Marketing Lead', 
+        texto: 'Resultados visibles en pocas semanas. Excelente soporte.', 
+        foto: 'https://i.pravatar.cc/150?img=45' 
+    },
+    {
+        id: 4,
+        nombre: 'Emiliano Rebolledo', 
+        cargo: 'Developer Senior', 
+        texto: 'Resultados visibles en pocas semanas. Excelente soporte.', 
+        foto: 'https://i.pravatar.cc/150?img=3' 
+    },
+    {
+        id: 5,
+        nombre: 'Claudia Navarrete', 
+        cargo: 'Developer Junior', 
+        texto: 'Resultados visibles en pocas semanas. Excelente soporte.', 
+        foto: 'https://i.pravatar.cc/150?img=5' 
+    }
+]; 
+
+export default testimonios;
